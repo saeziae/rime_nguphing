f724d25c464c2b692b28ac5e3abe47faa53c9836
saeziae <i@estela.cn>
2022-03-01 20:21:45 +0800

    add py script to make yaml dict

---
+++ makedict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import time
+lines=[]
+with open("nguphing.csv","r",encoding="utf-8") as f:
+  lines=f.readlines()
+lines=[x.split(",") for x in lines[1:]]
+with open("nguphing.dict.yaml","w",encoding="utf-8") as f:
+    f.write("""# Rime dictionary
+# encoding: utf-8
+---
+name: nguphing
+version: "{}"
+sort: by_weight
+use_preset_vocabulary: true
+import_tables:
+  - nguphing.word
+...
+""".format(time.strftime("%Y%m%d",time.localtime())))
+    for line in lines:
+        f.write("\t".join(line[0:3])+"\n")

67d7934cdc261f8c55875f2b70164b792fe5122f
saeziae <i@estela.cn>
2022-03-02 19:36:26 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -4,4 +4,5 @@ cp rime-essay/essay.txt rime-prelude/symbols.yaml rime-prelude/punctuation.yaml 
 rime_deployer --compile nguphing.schema.yaml
 rime_deployer --compile nguphing-shanghai.schema.yaml
 rm key_bindings.yaml punctuation.yaml symbols.yaml default.yaml essay.txt
-zip nguphing-$(date '+%Y%m%d').zip build/*.bin build/*.yaml
+zip nguphing-$(date '+%Y%m%d')-bin.zip build/*.bin build/*.yaml
+zip nguphing-$(date '+%Y%m%d')-yaml.zip *.yaml

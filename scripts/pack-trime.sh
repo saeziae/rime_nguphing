3f0fd6721b9f825942a3551fe2eabb278c8b1b9f
Estela ad Astra <i@estela.moe>
2024-01-11 19:25:45 +0100

    avoid double packing

--- scripts/pack-trime.sh
+++ scripts/pack-trime.sh
@@ -3,5 +3,5 @@ mkdir build
 export rime_dir="./build"
 curl -fsSL https://git.io/rime-install | rime_frontend=fcitx-rime bash -s -- :preset
 cp *.yaml build
-cd build
-zip -r nguphing-datapack.zip *
+#cd build
+#zip -r nguphing-datapack.zip *

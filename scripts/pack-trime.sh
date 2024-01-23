01316aa66eb3a0ca021f2d342724b68e8954e5d5
Estela ad Astra <i@estela.moe>
2024-01-23 16:37:11 +0100

    fix for ios?

--- scripts/pack-trime.sh
+++ scripts/pack-trime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 export rime_dir="./build"
-curl -fsSL https://git.io/rime-install | rime_frontend=fcitx-rime bash -s -- :preset
+curl -fsSL https://git.io/rime-install | bash -s -- :preset
 cp *.yaml build
 #cd build
 #zip -r nguphing-datapack.zip *

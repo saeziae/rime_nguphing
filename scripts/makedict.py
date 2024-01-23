3b1372c984911b5c54a9e137035cc168384ac4d7
Estela ad Astra <i@estela.moe>
2024-01-23 23:16:01 +0100

    upda vocabulary

--- scripts/makedict.py
+++ scripts/makedict.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 import time
 
-dicts = [["characters", "nguphing.char"],
-         ["words-legacy", "nguphing.word"],
+dicts = [["characters", "nguphing.char"]]
+
+for table in dicts:
+    lines = []
+    with open("vocabulary/"+table[0]+".csv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    lines = [x.rstrip("\n").split(",") for x in lines[1:]]
+    with open(table[1]+".dict.yaml", "w", encoding="utf-8") as f:
+        f.write("""# Rime dictionary
+# encoding: utf-8
+---
+name: {}
+version: "{}"
+sort: by_weight
+...
+""".format(table[1], time.strftime("%Y%m%d", time.localtime())))
+        for line in lines:
+            if line[0] != "#":
+                f.write(line[0]+"\t"+line[1]+"\t" +
+                        (line[3] if len(line) > 3 else "")+"\n")
+    print(table[1]+" converted!")
+
+###
+
+dicts = [["words-legacy", "nguphing.word"],
          ["words", "nguphing.words"],
          ["no-words", "nguphing.nowords"],
          ["map", "nguphing.map"],
-         ["phrases", "nguphing.phrases"],         
-         ]
+         ["phrases", "nguphing.phrases"]]
 
 for table in dicts:
     lines = []

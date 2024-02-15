3851bc120e75b862305c79f2af594800b927b411
Estela ad Astra <i@estela.moe>
2024-02-15 04:37:19 +0100

    update dict

--- scripts/makedict.py
+++ scripts/makedict.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import time
 
-dicts = [["characters", "nguphing.char"]]
+def generatedict(filenames,target,width=2):
+    chars = []
+    for filename in filenames:
+        with open("vocabulary/"+filename+".tsv", "r", encoding="utf-8") as f:
+            charlist = f.readlines()
+            for item in list(charlist)[1:]:
+                if item[0] == "#":
+                    continue
+                chars.append("\t".join(item.rstrip().split("\t")[0:width]))
 
-for table in dicts:
-    lines = []
-    with open("vocabulary/"+table[0]+".csv", "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    lines = [x.rstrip("\n").split(",") for x in lines[1:]]
-    with open(table[1]+".dict.yaml", "w", encoding="utf-8") as f:
+    with open(target+".dict.yaml", "w", encoding="utf-8") as f:
         f.write("""# Rime dictionary
 # encoding: utf-8
 ---
@@ -16,36 +19,13 @@ name: {}
 version: "{}"
 sort: by_weight
 ...
-""".format(table[1], time.strftime("%Y%m%d", time.localtime())))
-        for line in lines:
-            if line[0] != "#":
-                f.write(line[0]+"\t"+line[1]+"\t" +
-                        (line[3] if len(line) > 3 else "")+"\n")
-    print(table[1]+" converted!")
+""".format(target, time.strftime("%Y%m%d", time.localtime())))
+        f.write("\n".join(chars))
+    print(target+" converted!")
 
-###
+generatedict(["chars-supp","chars-main"],"nguphing.char",3)
+generatedict(["common-word"],"nguphing.common")
+generatedict(["words","words-legacy","no-words"],"nguphing.word")
+generatedict(["phrases"],"nguphing.phrases")
+generatedict(["map"],"nguphing.map")
 
-dicts = [["words-legacy", "nguphing.word"],
-         ["words", "nguphing.words"],
-         ["no-words", "nguphing.nowords"],
-         ["map", "nguphing.map"],
-         ["phrases", "nguphing.phrases"]]
-
-for table in dicts:
-    lines = []
-    with open("vocabulary/"+table[0]+".csv", "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    lines = [x.rstrip("\n").split(",") for x in lines[1:]]
-    with open(table[1]+".dict.yaml", "w", encoding="utf-8") as f:
-        f.write("""# Rime dictionary
-# encoding: utf-8
----
-name: {}
-version: "{}"
-sort: by_weight
-...
-""".format(table[1], time.strftime("%Y%m%d", time.localtime())))
-        for line in lines:
-            if line[0] != "#":
-                f.write("\t".join(line[0:2])+"\n")
-    print(table[1]+" converted!")
